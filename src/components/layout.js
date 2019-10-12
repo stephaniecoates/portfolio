f1eb82e695d2e4a0eec1113ae8d1f1b2a7143b69
Stephanie Coates <stcoates8@gmail.com>
2019-10-12 14:44:40 -0700

    feat(layout): simplified layout component, removed client side rendering

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import "./layout.css";
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -8,56 +7,25 @@ background-color: #faf9f7;
 height: 100%;
 `
 
-const Logo = styled.h1`
-padding: 30px;
-margin: 0px;
-text-decoration: none;
-width: 140px;
+const MenuLink = styled(Link)`
+padding: 5vh 2vw;
 `
-
-const LogoLink = styled(Link)`
-text-decoration: none;
-color: black; 
-letter-spacing: -8px;
-&:hover {
-  color: #8a9b7f;
-  text-decoration: none;
-}  
-`
-
 class Layout extends React.Component {
   render() {
     const { children } = this.props
     const header = (
-      <div>
-        <Logo>
-          <LogoLink to="/">
-            <div style={{fontFamily: 'Satisfy', fontWeight: 'bold'}}>SC</div>
-          </LogoLink>
-        </Logo>
-        <nav>
-          <input type="checkbox" className="menu-open" name="menu-open" id="menu-open"/>
-          <label className="menu-open-button" htmlFor="menu-open">
-            <span className="hamburger hamburger-1"></span>
-            <span className="hamburger hamburger-2"></span>
-            <span className="hamburger hamburger-3"></span>
-          </label>
-         <Link to="/blog" className="menu-item"> blog</Link>
-         <Link to="/#contact" className="menu-item"> contact</Link>
-         <Link to="/#projects" className="menu-item"> projects</Link>
-          <Link to="/#about" className="menu-item">about</Link>
-      </nav>
+      <div style={{display: 'flex', justifyContent: 'flex-end', paddingRight: '15px'}}>
+         <MenuLink to="/">home</MenuLink>
+          <MenuLink to="/timeline">timeline</MenuLink>
+         <MenuLink to="/projects">projects</MenuLink>
+         <MenuLink to="/contact">contact</MenuLink>
+         <MenuLink to="/blog"> blog</MenuLink>
       </div>
     )
     return (
       <Container>
         <div>{header}</div>
         <div>{children}</div>
-        <div>
-          © {new Date().getFullYear()}, Built with &hearts; with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </div>
       </Container>
     )
   }
